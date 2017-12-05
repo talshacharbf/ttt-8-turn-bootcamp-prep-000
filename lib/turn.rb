9d2091f0aba7b5b41fd0a65b4f8ecf0d96a7b015
tal shachar <tal.shachar@buzzfeed.com>
2017-12-05 05:00:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,24 @@ def display_board(board)
  puts "-----------"
  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index)
+move = nil
+  if index.between?(0,8) && position_taken?(board, index)
+    move = true
+  else
+    move = false
+  end
+  move
+end
+
+
+def position_taken?(board, index)
+  position = nil
+    if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    position = true
+    else
+    position = false
+    end
+    position
+end
