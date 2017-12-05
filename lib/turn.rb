83e1db2fb47390b0bf59d9734dd021c3720abfd6
tal shachar <tal.shachar@buzzfeed.com>
2017-12-05 05:04:09 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,6 +31,6 @@ def input_to_index(user_input)
   converted_input = "#{user_input}".to_i - 1
 end
 
-def move(array, converted_input, character = "X")
-  array[converted_input] = character
+def move(board, converted_input, character = "X")
+  board[converted_input] = character
 end
